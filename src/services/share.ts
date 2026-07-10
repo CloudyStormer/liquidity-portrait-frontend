e92f1f40c109b82b1e54f7c41050dc64dfa0d013
CloudyStormer <CloudyStormer@users.noreply.github.com>
2026-07-10 09:48:50 +0800

    Use native WeChat share menu options

--- src/services/share.ts
+++ src/services/share.ts
@@ -2,6 +2,22 @@ import Taro, { useDidShow, useShareAppMessage, useShareTimeline } from '@tarojs/
 
 const SHARE_TITLE = '\u8bc1\u4ef6\u7167\u751f\u6210\u5668'
 const SHARE_PATH = '/pages/index/index'
+const SHARE_MENU_OPTIONS = {
+  withShareTicket: true,
+  menus: ['shareAppMessage', 'shareTimeline'],
+  showShareItems: ['shareAppMessage', 'shareTimeline', 'wechatFriends', 'wechatMoment']
+}
+
+type WeappShareMenuApi = {
+  showShareMenu?: (option: {
+    withShareTicket?: boolean
+    menus?: string[]
+    showShareItems?: string[]
+    success?: () => void
+    fail?: () => void
+    complete?: () => void
+  }) => void
+}
 
 type ShareOptions = {
   title?: string
@@ -18,10 +34,14 @@ export function useWechatShare(options: ShareOptions = {}) {
 
   useDidShow(() => {
     if (Taro.getEnv() !== Taro.ENV_TYPE.WEAPP) return
-    Taro.showShareMenu({
-      withShareTicket: true,
-      showShareItems: ['shareAppMessage', 'shareTimeline']
-    }).catch(() => undefined)
+    const wxApi = (globalThis as typeof globalThis & { wx?: WeappShareMenuApi }).wx
+
+    wxApi?.showShareMenu?.({
+      ...SHARE_MENU_OPTIONS,
+      fail: () => undefined
+    })
+
+    Taro.showShareMenu(SHARE_MENU_OPTIONS as Taro.showShareMenu.Option).catch(() => undefined)
   })
 
   useShareAppMessage(() => ({
